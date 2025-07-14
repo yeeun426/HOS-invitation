17bfe176373f36184a0b5ba108272cf84ffaa78d
yeeun426 <thsudkcla7@naver.com>
2025-07-15 01:40:04 +0900

    Feat : 초대장 UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import Invitation from "./components/Invitation";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isClick, setIsClick] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="background">
+      <div className="container">
+        <h2>Welcome to SLOW CLUB</h2>
+        <div className="text">
+          안녕하세요.
+          <p> HOOKOFSPACE의 SLOW CLUB에 당신을 초대합니다.</p>
+          <p>
+            SLOW CLUB은 예술을 기반으로, 사람들과 느리지만 깊게 스며둘수 있게
+            하는 소셜클럽입니다.
+          </p>
+          <p>
+            예술은 우리와 정말 가까이 있습니다.
+            <br /> 하지만, 우리는 모르고 지나갑니다.
+          </p>
+          <p>
+            HOOKOFSPACE는 일상속에서 예술을 느낄수 있게 신진작가와 함께 의미있고
+            깊은 기획을 진행합니다.
+          </p>
+          그럼, 우리 HUAMDONG에서 만나요! <br /> THANK YOU
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Invitation.tsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./Invitation.css";
+
+const Invitation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? "open" : ""}`} onClick={handleOpen}>
+      <div className="envelope-cover" />
+      <div className="letter">
+        <p className="letter-text">
+          친애하는 예은님, <br />이 초대장을 받아주세요 💌
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Invitation;
