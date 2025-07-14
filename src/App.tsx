71564b1be07ac61ce9970976816e391fe38c894d
yeeun426 <thsudkcla7@naver.com>
2025-07-15 01:52:46 +0900

    Feat : 배경, 글꼴 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,32 @@ function App() {
   return (
     <div className="background">
       <div className="container">
-        <h2>Welcome to SLOW CLUB</h2>
+        <h2>
+          You're Invited to
+          <br />
+          SLOW CLUB
+        </h2>
         <div className="text">
-          안녕하세요.
-          <p> HOOKOFSPACE의 SLOW CLUB에 당신을 초대합니다.</p>
           <p>
-            SLOW CLUB은 예술을 기반으로, 사람들과 느리지만 깊게 스며둘수 있게
-            하는 소셜클럽입니다.
+            안녕하세요.
+            <br />
+            SLOW CLUB에 당신을 초대합니다.
           </p>
           <p>
-            예술은 우리와 정말 가까이 있습니다.
-            <br /> 하지만, 우리는 모르고 지나갑니다.
+            SLOW CLUB은 예술을 기반으로, 사람들과 느리지만 깊게 스며들 수 있게
+            하는 소셜 클럽입니다.
           </p>
           <p>
-            HOOKOFSPACE는 일상속에서 예술을 느낄수 있게 신진작가와 함께 의미있고
-            깊은 기획을 진행합니다.
+            예술은 우리와 정말 가까이 있습니다. <br />
+            하지만, 우리는 모르고 지나갑니다.
           </p>
-          그럼, 우리 HUAMDONG에서 만나요! <br /> THANK YOU
+          <p>
+            일상 속에서 예술을 느낄 수 있게
+            <br />
+            신진작가와 함께 의미있고 깊은 기획을 진행합니다.
+          </p>
+          <p>그럼, 우리 HUAMDONG에서 만나요!</p>
+          <p>THANK YOU</p>
         </div>
       </div>
     </div>
