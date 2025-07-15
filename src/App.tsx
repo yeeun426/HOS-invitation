9e6102b2facb8919478331dd4dc8481921e19a75
yeeun426 <thsudkcla7@naver.com>
2025-07-15 16:28:01 +0900

    Feat : 크기 조절, 애니메이션 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,39 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [isClick, setIsClick] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
+
+  useEffect(() => {
+    if (isClick) {
+      const timer = setTimeout(() => setFadeIn(true), 50);
+      return () => clearTimeout(timer);
+    }
+  }, [isClick]);
+
   return (
-    <div className="background">
-      <div className="container">
-        <h2>
-          You're Invited to
-          <br />
-          SLOW CLUB
-        </h2>
-        <div className="text">
-          <p>
-            안녕하세요.
-            <br />
-            SLOW CLUB에 당신을 초대합니다.
-          </p>
-          <p>
-            SLOW CLUB은 예술을 기반으로, 사람들과 느리지만 깊게 스며들 수 있게
-            하는 소셜 클럽입니다.
-          </p>
-          <p>
-            예술은 우리와 정말 가까이 있습니다. <br />
-            하지만, 우리는 모르고 지나갑니다.
-          </p>
-          <p>
-            일상 속에서 예술을 느낄 수 있게
-            <br />
-            신진작가와 함께 의미있고 깊은 기획을 진행합니다.
-          </p>
-          <p>그럼, 우리 HUAMDONG에서 만나요!</p>
-          <p>THANK YOU</p>
-        </div>
+    <div className="background" onClick={() => setIsClick(true)}>
+      <div className={`container ${fadeIn ? "show" : ""}`}>
+        {isClick && (
+          <>
+            <h2>SLOW CLUB</h2>
+            <div className="text">
+              <p>
+                초대할게요
+                <br />
+                아트 소셜 클럽
+              </p>
+              <p>느리지만 깊은, 그러한 시간</p>
+              <p>
+                장소 : 후암동
+                <br />
+                날짜 : 2025.07.24
+              </p>
+              <p>그럼, 우리 곧 만나요!</p>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
